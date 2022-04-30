2a817db890f6b635befe405f5e0321917f63270a
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2022-04-30 20:41:47 +0900

    comp flask

--- fl.py
+++ fl.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template,request
 from flask_socketio import SocketIO,emit
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="./templates/images")
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app,cors_allowed_origins="*")
 @app.route("/")
 def hello():
     return render_template("index.html")
 
-@app.route("/good",methods=["POST","GET"])
+@app.route("/good")
 def good():
-    if request.method == "POST":
-        name = request.form["name"]
-        emit("icon_update",{"icon_src":name})
+    if request.method == "GET":
+        #print(request.args)
+        name = request.args["name"]
+        print(name)
+        socketio.emit("icon_update",{"icon_src":name})
+        return "ok"
 socketio.run(app,debug=True)
